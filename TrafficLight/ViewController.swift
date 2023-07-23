03b97f0fef740be4cbf03135bec15d813ef37029
kivetat-tatevik <kivetat.85@yandex.ru>
2023-07-23 14:08:06 +0200

    stack and view done

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    private let radiusForCircle = CGFloat(60)
+    private let lightOff = CGFloat(0.3)
+    private let lightON = CGFloat(1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLightView.layer.cornerRadius = radiusForCircle
+        greenLightView.layer.cornerRadius = radiusForCircle
+        yellowLightView.layer.cornerRadius = radiusForCircle
+        redLightView.alpha = lightOff
+        yellowLightView.alpha = lightOff
+        greenLightView.alpha = lightOff
     }
 
-
+    @IBAction func changeTheLightWhenPressed(_ sender: UIButton) {
+        
+        sender.setTitle("NEXT", for: .normal)
+        
+        if redLightView.alpha == lightON {
+            yellowLightView.alpha = lightON
+            redLightView.alpha = lightOff
+            
+        } else if yellowLightView.alpha == lightON {
+            greenLightView.alpha = lightON
+            yellowLightView.alpha = lightOff
+            
+        } else {
+            redLightView.alpha = lightON
+            greenLightView.alpha = lightOff
+        }
+    }
 }
 
